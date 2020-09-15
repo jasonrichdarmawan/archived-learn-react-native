6ae14d61fa4fc9d232c8ae6a4797d75105e3302e
kidfrom <jason.onggo@tempatkerja.com>
2020-09-15 13:50:44 +0700

    add drawer

--- state/App.js
+++ state/App.js
@@ -4,7 +4,8 @@ import {Provider, useDispatch, useSelector} from 'react-redux';
 import store from './store';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {
@@ -16,26 +17,25 @@ import SignIn from './src/features/SignIn';
 import Dashboard from './src/features/Dashboard';
 import Album from './src/features/Album';
 
-import {Button} from 'react-native';
+import {Button, View, Text} from 'react-native';
 
 const SignInComponent = () => null;
 const Tab = createBottomTabNavigator();
-function MyTabs({navigation}) {
+function MyTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Dashboard" component={Dashboard} />
       <Tab.Screen name="Album" component={Album} />
       <Tab.Screen
-        options={{
-          title: 'Sign In',
+        options={({navigation}) => ({
           tabBarButton: () => (
             <Button
               onPress={() => navigation.navigate('Sign In')}
               title="Sign In"
             />
           ),
-        }}
+        })}
         name="SignInComponent"
         component={SignInComponent}
       />
@@ -53,11 +53,38 @@ function MyStack() {
   );
 }
 
+const Feed = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Feed Screen</Text>
+  </View>
+);
+
+const Article = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Article Screen</Text>
+  </View>
+);
+
+const Drawer = createDrawerNavigator();
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        options={{title: 'Home'}}
+        name="MyStack"
+        component={MyStack}
+      />
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <MyStack />
+        <MyDrawer />
       </NavigationContainer>
     </Provider>
   );

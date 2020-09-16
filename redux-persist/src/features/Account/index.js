3e6fae78632bfbfce9de2fb90a7b7bdbcd93ce41
kidfrom <jason.onggo@tempatkerja.com>
2020-09-16 12:23:48 +0700

    add sign out button

--- redux-persist/src/features/Account/index.js
+++ redux-persist/src/features/Account/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import {View, Text, Button} from 'react-native';
+import {useDispatch} from 'react-redux';
+import { signOut } from '../AuthState/AuthStateSlice';
 
 export default function Account() {
+  const dispatch = useDispatch();
   return (
     <View>
       <Text>Account</Text>
+      <Button title="Sign Out" onPress={() => dispatch(signOut())} />
     </View>
   );
 }
